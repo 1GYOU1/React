0d58fd62d347a227aa998428b1fef2ffab129560
1GYOU1 <90018379+1GYOU1@users.noreply.github.com>
2022-12-11 17:33:06 +0900

    2022-12-11 react study

---
+++ React.js/hello-react/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    const name = "리액트";
+    return <div className="react">{name}</div>;
+  }
+}
+
+export default App;
