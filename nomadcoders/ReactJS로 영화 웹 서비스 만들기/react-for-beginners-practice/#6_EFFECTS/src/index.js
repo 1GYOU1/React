d437aad4e1d93e8f6db498a8138d07ab95df143d
1GYOU1 <90018379+1GYOU1@users.noreply.github.com>
2023-02-06 23:19:07 +0900

    2023-02-06 react study

--- nomadcoders/ReactJS로 영화 웹 서비스 만들기/react-for-beginners-practice/#6_EFFECTS/src/index.js
+++ nomadcoders/ReactJS로 영화 웹 서비스 만들기/react-for-beginners-practice/#6_EFFECTS/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import ToDoListpartOne from './ToDoListpartOne';
+import CoinTracker from './CoinTracker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    {/*<App />*/}{/* #6 EFFECTS */}
-    <ToDoListpartOne />
-  </>
+    <>
+        {/*<App />*/}{/* #6 EFFECTS */}
+        {/* <ToDoListpartOne /> */}{/* #7.1 ~ #7.2 */}
+        <CoinTracker />{/* #7.3 Coin Tracker */}
+    </>
 );
