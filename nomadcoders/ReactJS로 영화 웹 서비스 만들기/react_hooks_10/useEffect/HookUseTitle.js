355a91146b843dd58e252001fa574ba9f3e7a879
1GYOU1 <90018379+1GYOU1@users.noreply.github.com>
2023-03-21 00:12:43 +0900

    2023-03-21 useEffect 활용 hook

--- nomadcoders/ReactJS로 영화 웹 서비스 만들기/react_hooks_10/useEffect/HookUseTitle.js
+++ nomadcoders/ReactJS로 영화 웹 서비스 만들기/react_hooks_10/useEffect/HookUseTitle.js
@@ -1,21 +1,22 @@
 /*
-#2.1 useTitle
-보통은 helmet이라는 것을 사용하지만, hook으로 만들어보기 !
+    #2.1 useTitle
+    보통은 helmet이라는 것을 사용하지만, hook으로 만들어보기 !
 */
 import React, { useEffect, useState }from 'react';
 
 const useTitle = initialTitle => {
+                  //setTitle은 Loading... 기본값이 됨.
     const [title, setTitle] = useState(initialTitle);
     const updateTitle = () => {
         const htmlTitle = document.querySelector("title");
         htmlTitle.innerText = title;
     }
     useEffect(updateTitle, [title]);
-    return setTitle;
+    return setTitle;//titleUpdater과 동일한 값을 가짐.
 };
 const HookUseTitle = () => {   
     const titleUpdater = useTitle('Loading...');
-    setTimeout(() => titleUpdater("Home"), 5000);
+    setTimeout(() => titleUpdater("Home"), 5000);//5초 뒤 "Home"으로 타이틀 업데이트
     return (
         <div>
             <h1>useTitle</h1>
