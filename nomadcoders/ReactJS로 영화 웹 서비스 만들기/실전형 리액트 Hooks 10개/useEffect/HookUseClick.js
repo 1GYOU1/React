71075ade1c3a67c5f72a6f6888cc46058f1da483
1GYOU1 <90018379+1GYOU1@users.noreply.github.com>
2023-03-21 21:47:07 +0900

    2023-03-21 react hook study

---
+++ nomadcoders/ReactJS로 영화 웹 서비스 만들기/실전형 리액트 Hooks 10개/useEffect/HookUseClick.js
@@ -0,0 +1,49 @@
+/*
+#2.2 useClick
+
+작동 순서
+1. componentWillMount
+: reder 전에 componentWillMount 실행, => element 의 click 이벤트 없애줌
+
+2. render
+: 페이지가 redering 됨
+3. componentDidMount
+redering이 된 후에 componentDidMount 실행 => click 이벤트 등록 'sayHello'
+
+4. [] 의사용
+dependency 에 아무것도 들어가있지 않음 => 페이지가 redering 될 때 한 번만 동작
+
+결과 : 클릭 이벤트 지우고 - 렌더링 하고 - 클릭이벤트 등록하고 - 한번만 로딩되니까 중복으로 클릭이벤트 등록 될 일 없음
+*/
+import React, { useEffect, useRef } from 'react';
+
+const useClick = (onClick) => {
+	const ref = useRef();
+	useEffect(() => {
+	const element = ref;
+	if (element.current) {
+	element.current.addEventListener("click", onClick);
+	}
+	return () => {
+		if (element.current) {
+			element.current.removeEventListener("click", onClick);
+		}
+	};
+	}, []);
+	if (typeof onclick !== "object") {
+		return;
+	}
+		return ref;
+	};
+
+const HookUseClick = () => {
+	const sayHello = () => console.log("say hello");
+	const title = useClick(sayHello);
+	return (
+		<div>
+			<h1 ref={title}>Hi</h1>
+		</div>
+	);
+};
+
+export default HookUseClick;
